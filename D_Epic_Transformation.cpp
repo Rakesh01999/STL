1757dce1aa8ae0bd1787cb155a0454797bb8e449
Rakesh Biswas <rbiswas01999@gmail.com>
2025-07-12 13:36:49 +0600

    D_Epic_Transformation added

---
+++ D_Epic_Transformation.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <unordered_map>
+#include <algorithm>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+
+        std::unordered_map<int, int> freq;
+        freq.reserve(n); // Avoid rehashing
+
+        int maxFreq = 0;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            scanf("%d", &x);
+            freq[x]++;
+            if (freq[x] > maxFreq)
+                maxFreq = freq[x];
+        }
+
+        if (maxFreq > n - maxFreq)
+            printf("%d\n", 2 * maxFreq - n);
+        else
+            printf("%d\n", n % 2);
+    }
+
+    return 0;
+}
